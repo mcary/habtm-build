0cef4b6211889eb915d2acbfeeb6210976f18073
Marcel M. Cary <marcel@oreilly.com>
2014-06-01 10:44:38 -0700

    Make build/save work by calling foo.save

--- spec/models/foo_items_foos_spec.rb
+++ spec/models/foo_items_foos_spec.rb
@@ -4,6 +4,7 @@ describe "FooItem's Foos" do
   it "gets filled in with foo.foo_items.build" do
     foo = Foo.create!(name: "Test foo")
     foo_item = foo.foo_items.build(name: "Test foo item")
+    foo.save!
     foo_item.save!
     p foo_item.foos
     p foo.foo_items
