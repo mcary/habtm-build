c4891edc88b066bb0c4b33dab387b92b3a846fd1
Marcel M. Cary <marcel@oreilly.com>
2014-06-01 08:40:03 -0700

    Create pristine Rails 4.0.5 app with --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Habtm::Application.config.secret_key_base = '5dfec65d0fc3f48ff1bf9588c867fefd264936d231cd7e7aa6cd79ebc7580b077dfae59691a73127f4f64a48ae72ef7d904f9b3b7126da0de574253d3f50f037'
