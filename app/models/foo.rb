6dd0d29caf230da113d0a3479d92ce48c044a174
Marcel M. Cary <marcel@oreilly.com>
2014-06-01 19:55:52 -0700

    Override foos.foo_items.build to add foo to foo_item.foos

--- app/models/foo.rb
+++ app/models/foo.rb
@@ -1,3 +1,11 @@
 class Foo < ActiveRecord::Base
-  has_and_belongs_to_many :foo_items
+  has_and_belongs_to_many :foo_items do
+    def build(*args)
+      super.tap do |target|
+        inverse_of = :foos
+        # Add the corresponding foo to the foo_item
+        target.public_send(inverse_of) << proxy_association.owner
+      end
+    end
+  end
 end
