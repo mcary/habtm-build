7765e17b9647573b2476b86b7908a5fdde926091
Marcel M. Cary <marcel@oreilly.com>
2014-06-01 20:11:17 -0700

    Set inverse_of to help a foo_item know about its foo
    This approach fails with:
    
        Failure/Error: foo = Foo.create!(name: "Test foo")
        ArgumentError:
          Unknown key: inverse_of
        # ./app/models/foo.rb:2:in `<class:Foo>'
        # ./app/models/foo.rb:1:in `<top (required)>'
        # ./spec/models/foo_items_foos_spec.rb:5:in
            `block (2 levels) in <top (required)>'

--- app/models/foo.rb
+++ app/models/foo.rb
@@ -1,3 +1,3 @@
 class Foo < ActiveRecord::Base
-  has_and_belongs_to_many :foo_items
+  has_and_belongs_to_many :foo_items, inverse_of: :foos
 end
--- app/models/foo_item.rb
+++ app/models/foo_item.rb
@@ -1,3 +1,3 @@
 class FooItem < ActiveRecord::Base
-  has_and_belongs_to_many :foos
+  has_and_belongs_to_many :foos, inverse_of: :foo_items
 end
